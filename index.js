cca9a5ddedd556e4e8787ec41fb0e9d19eb55a24
Evan Teems <evanteems40@yahoo.com>
2021-02-13 23:03:35 -0800

    README template init

---
+++ index.js
@@ -0,0 +1,79 @@
+const inquirer = require("inquirer");
+
+const fs = require("fs");
+
+const utils = require("utilis");
+
+// README template links here
+const ReadMeGenerator = require('.src/ReadMeTemplate');
+
+const ReadMeTemplate = require("./src/ReadMeTemplate");
+const { async } = require("rxjs");
+
+//Function that writes the README file
+const createFile = utils.promisify(fs.write);
+
+// Creating an array of Questions for the user to input
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'Username',
+            message: 'Please enter github username: (Required)'
+        },
+        {
+            type: 'input',
+            name: 'project-title',
+            message: "What is the name of your project title?"
+        },
+        {
+            type: 'input',
+            name: 'Project Description',
+            message: 'Please Describe your project:'
+        },
+        {
+            type: 'input',
+            name: 'Install',
+            message: 'What are the steps required to install your project?'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide instruction and examples for usage. Included screenshotsas needed:'
+        },
+        {
+           type: 'input',
+           name: 'Credit',
+           message: 'Please list all collaborators. If n/a leave blank:' 
+        },
+        {
+            type: 'list',
+            name: 'lisence',
+            message: 'Choose the license for your project:',
+            choices: ['GNU AGPL', 'GNU LGPL', 'Mozilla', 'Apache', 'MIT', 'Boost Software', 'The Unlicense']
+        },
+        {
+            type: 'input',
+            name: 'features',
+            message: 'Please list your project features/ languages:'
+        }
+    ])
+};
+
+
+// Create a function to initalize the application
+async function init() {
+    try {
+        const data = await promptUser();
+        const createContent = ReadMeTemplate(data);
+
+        await createFile('./dist/README.md', createContent);
+        console.log('README.md successfully created');
+    } catch(err) {
+        console.log(err);
+    }
+};
+
+
+// Function that calls the initalized application
+init();
---
+++ src/ReadMeTemplate.js
@@ -0,0 +1,9 @@
+function ReadMeTemplate(data) {
+    return `
+    <h1 align='center'>${data.projectTitle}</h1>
+    
+    ##DESCRIPTION
+        ${data.description}
+        
+    `
+}
